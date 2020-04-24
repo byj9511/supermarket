451434a0a31f97e7a21f23134f19cbdd6f1cfe0b
byj <18621711850@163.com>
2020-04-24 22:12:10 +0800

    搭建人人开源项目，准备逆向sql

---
+++ vue_test/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app" class="test">
+    <img src="./assets/logo.png">
+      <router-view/>
+<!--    路径视图，其内部的内容会随着路径二被替换-->
+<!--    视图功能-->
+<!--    不能使用原生的a标签-->
+<!--    <a href="/hello">去hello</a>-->
+<!--    要用router进行路由-->
+      <router-link to="/hello">去hello</router-link>
+      <router-link to="/elementUI">去ElementUIdemo</router-link>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue_test/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Hello from '@/components/Hello'
+import ElementUIDemo from '@/components/ElementUIDemo'
+import mytable from "../components/mytable";
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/hello',
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path: '/elementUI',
+      name: 'ElementUIDemo',
+      component: ElementUIDemo
+    },
+    {
+      path: '/mytable',
+      name: 'mytable',
+      component: mytable
+    }
+  ]
+})
